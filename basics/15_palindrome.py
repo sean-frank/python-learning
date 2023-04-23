c31ae92d8dbe5ec720909e4bfe14f233c91be9c2
Sean Frank <seanpf522@gmail.com>
2023-04-22 20:06:41 -0400

    Added excersise 15

---
+++ basics/15_palindrome.py
@@ -0,0 +1,18 @@
+"""
+Write a Python function that checks whether a passed string is a palindrome or not. Go to the editor
+Note: A palindrome is a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+"""
+word = "nurses run"
+
+def check(word):
+    removed_spaces = word.lower().replace(" ","")[::-1]
+    reversed_word = removed_spaces[::-1]
+    return reversed_word == removed_spaces
+    
+validator = check(word)
+if validator == True:
+    print("The word %s is a palindrome." %word)
+else:
+    print(f"The word {word} is not a palindrome")
+
+
